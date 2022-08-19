4ce0290d198a866e9d8a2119836fc6116c78fcd2
Aleksandr <belyakov71@yandex.ru>
2022-08-19 04:28:48 +0300

    Домашняя работа. Задача №19

---
+++ HomeWorkTask19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 19. Программа принимает на вход пятизначное число
+// проверяет является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число для проверки: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int num5 = numA / 10000;
+int num1 = numA % 10;
+int num4 = numA / 1000 % 10;
+int num2 = numA % 100 / 10;
+
+if (num1 == num5 && num2 == num4)
+    Console.WriteLine("Число является полиндромом");
+else
+    Console.WriteLine("Число не является полиндромом");
