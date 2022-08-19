d0e649591f2fb8f85e3f3bade712b3699962b81a
Aleksandr <belyakov71@yandex.ru>
2022-08-19 05:04:21 +0300

    Домашняя работа. Задача №23

---
+++ HomeWorkTask23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает на вход число (N)
+// выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{   
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i}\t{i * i * i}");
+    }
+}
+else Console.WriteLine("Число имеет не корректное значение");
